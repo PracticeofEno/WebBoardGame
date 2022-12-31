8e4b8c624db77e85ba5134e00e98f8dbf190ba7f
Eno <k1990121@gmail.com>
2022-12-31 15:24:34 +0900

    websocket example

--- front_nextjs/pages/index.js
+++ front_nextjs/pages/index.js
@@ -1,8 +1,30 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css';
 import MainPage from '../components/layout'
+import SocketIOClient from "socket.io-client";
+import { useState, useEffect, useRef } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    console.log("execute useEffect");
+    // connect to socket server
+    const socket = SocketIOClient.connect("http://localhost:5050/game");
+
+    // log socket connection
+    socket.on("connect", () => {
+      console.log("SOCKET CONNECTED!", socket.id);
+    });
+
+    // update chat on new message dispatched
+    socket.on("message", (message) => {
+      chat.push(message);
+      setChat([...chat]);
+    });
+
+    // socket disconnet onUnmount if exists
+    if (socket) return () => socket.disconnect();
+  }, []);
+
   return (
     <MainPage>
     </MainPage>

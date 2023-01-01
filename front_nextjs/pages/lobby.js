82b65e192ff2ceb11e2c711832496758309f14de
Eno <k1990121@gmail.com>
2023-01-01 16:42:25 +0900

    bug fix

--- front_nextjs/pages/lobby.js
+++ front_nextjs/pages/lobby.js
@@ -2,29 +2,22 @@ import BigBox from "../components/lobby/BigBox";
 import TopPadding from "../components/lobby/TopPadding";
 import Chat from "../components/lobby/Chat";
 import Middle from "../components/lobby/MiddlePannel";
+import io from "socket.io-client";
+import { useEffect } from "react";
 
 export default function Lobby() {
-  function useSocket(url) {
-    const [socket, setSocket] = useState(null);
-    useEffect(() => {
-      fetch('http://localhost:5050/game').finally(() => {
-        const socketio = io();
-        socketio.on("connect", () => {
-          console.log("connect");
-          socketio.emit("hello");
-        });
-        socketio.on("disconnect", () => {
-          console.log("disconnect");
-        });
-        setSocket(socketio);
-      });
-      function cleanup() {
-        socket.disconnect();
-      }
-      return cleanup;
-    }, []);
-    return socket;
-  }
+  useEffect(() => {
+    // connect to socket server
+    const socket = io.connect("http://localhost:5050/game");
+
+    // log socket connection
+    socket.on("connect", () => {
+      console.log("SOCKET CONNECTED!", socket.id);
+    });
+
+    // socket disconnet onUnmount if exists
+    if (socket) return () => socket.disconnect();
+  }, []);
 
   return (
     <BigBox>

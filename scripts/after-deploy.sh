006985249c9df30ab5e2bc3a1c5582295f2e6f68
PracticeofEno <57505385+PracticeofEno@users.noreply.github.com>
2023-02-06 20:11:15 +0900

    Update after-deploy.sh

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -3,6 +3,9 @@ REPOSITORY=/home/ubuntu/test
 
 cd $REPOSITORY
 docker rm frontend backend 
+echo "rm success" >> log
 docker rmi frontend:42 backend:42
+echo "rmi success" >> log
 docker system prune -f
+echo "prune success" >> log
 docker-compose up -d

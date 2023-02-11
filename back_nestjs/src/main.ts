69bbb0ce3feca1fac1ce091bf28d14ed9ff869df
haha <haha@gmail.com>
2023-02-11 14:34:14 +0900

    aa

--- back_nestjs/src/main.ts
+++ back_nestjs/src/main.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './util/swagger';
@@ -10,16 +9,3 @@ async function bootstrap() {
   await app.listen(5000);
 }
 bootstrap();
-=======
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { setupSwagger } from './util/swagger';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  setupSwagger(app);
-  await app.listen(5000);
-}
-bootstrap();
->>>>>>> tmp
--- back_nestjs/src/user/user.repository.ts
+++ back_nestjs/src/user/user.repository.ts
@@ -1,68 +1,3 @@
-<<<<<<< HEAD
-import { NotFoundException, HttpException } from "@nestjs/common";
-import { CustomRepository } from "../configs/typeorm-ex.decorator";
-import { Repository } from "typeorm";
-import { User } from "../entity/user.entity"
-import * as bcrypt from "bcrypt";
-
-@CustomRepository(User)
-export class UserRepository extends Repository<User> {
-
-	async findById(id: number): Promise<User> {
-		const found = await this.findOne({
-			where: {
-				id: id,
-			},
-		})
-		return found;
-	}
-
-	async findByUserId(user_id: string): Promise<User> {
-		const found = await this.findOne({
-			where: {
-				user_id: user_id,
-			},
-		})
-		return found;
-	}
-
-	async findByNickname(nickname: string): Promise<User> {
-		const found = await this.findOne({
-			where: {
-				nickname: nickname,
-			},
-		})
-		return found;
-	}
-
-	async addUser(user_id: string, password: string): Promise<User> {
-		const salt = await bcrypt.genSalt();
-		const hash = await bcrypt.hash(password, salt);
-		let user = new User();
-		user.nickname = "";
-		user.password = hash;
-		user.user_id = user_id;
-		user.win = 0;
-		user.lose = 0;
-		user.status = 0;
-		await this.save(user);
-		return user;
-	}
-
-	async updateNickname(id: number, nickname: string): Promise<User | null> {
-		let user = await this.findByNickname(nickname);
-		let tmp = await this.findById(id);
-		if (user == null) {
-			tmp.nickname = nickname;
-			await this.update(id, tmp);
-			return tmp;
-		}
-		else {
-			return null;
-		}
-	}
-}
-=======
 import { NotFoundException, HttpException } from "@nestjs/common";
 import { CustomRepository } from "../configs/typeorm-ex.decorator";
 import { Repository } from "typeorm";
@@ -126,4 +61,3 @@ export class UserRepository extends Repository<User> {
 		}
 	}
 }
->>>>>>> tmp
